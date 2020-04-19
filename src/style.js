4dcc46d745b81355d785b263658f015bd1d46ea5
Jonathan Troop <40128411+troopaloop8@users.noreply.github.com>
2020-04-19 13:25:40 +0900

    Update style.js

--- src/style.js
+++ src/style.js
@@ -17,52 +17,7 @@ module.exports = {
       pointerEvents: 'none',
       outline: 'none',
       backgroundColor: 'black',
-      backgroundImage: url('https://i.imgur.com/uBz9rp9.png')
-      border: 'solid 1px rgba(255,255,255,1)',
-      borderRadius: 6
-    }
-  },
-  stop: {
-    hover: {
-      backgroundColor: 'white'
-    },
-    button: {
-      width: '50',
-      height: '44',
-      cursor: 'pointer',
-      pointerEvents: 'none',
-      outline: 'none',
-      backgroundColor: 'black',
-      border: 'solid 1px rgba(255,255,255,1)',
-      borderRadius: 6
-    }
-  },
-  pause: {
-    hover: {
-      backgroundColor: 'white'
-    },
-    button: {
-      width: '50',
-      height: '44',
-      cursor: 'pointer',
-      pointerEvents: 'none',
-      outline: 'none',
-      backgroundColor: 'black',
-      border: 'solid 1px rgba(255,255,255,1)',
-      borderRadius: 6
-    }
-  },
-  resume: {
-    hover: {
-      backgroundColor: 'white'
-    },
-    button: {
-      width: '50',
-      height: '44',
-      cursor: 'pointer',
-      pointerEvents: 'none',
-      outline: 'none',
-      backgroundColor: 'black',
+      backgroundImage: url('https://i.imgur.com/uBz9rp9.png'),
       border: 'solid 1px rgba(255,255,255,1)',
       borderRadius: 6
     }
